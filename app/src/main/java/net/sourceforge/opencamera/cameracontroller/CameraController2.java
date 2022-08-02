08e722e4e1c1d9eb0d892ec3ab891906548b3360
Mark Harman <mark.harman.apps@gmail.com>
2022-08-02 21:26:34 +0100

    Make setting of CONTROL_CAPTURE_INTENT consistent with the template.

--- app/src/main/java/net/sourceforge/opencamera/cameracontroller/CameraController2.java
+++ app/src/main/java/net/sourceforge/opencamera/cameracontroller/CameraController2.java
@@ -7107,8 +7107,9 @@ public class CameraController2 extends CameraController {
                     Log.d(TAG, "imageReader surface: " + imageReader.getSurface().toString());
                 }
 
+                // n.b., takePictureBurst() not currently called if previewIsVideoMode==true, but have put this code here for possible future use
                 CaptureRequest.Builder stillBuilder = camera.createCaptureRequest(previewIsVideoMode ? CameraDevice.TEMPLATE_VIDEO_SNAPSHOT : CameraDevice.TEMPLATE_STILL_CAPTURE);
-                stillBuilder.set(CaptureRequest.CONTROL_CAPTURE_INTENT, CaptureRequest.CONTROL_CAPTURE_INTENT_STILL_CAPTURE);
+                stillBuilder.set(CaptureRequest.CONTROL_CAPTURE_INTENT, previewIsVideoMode ? CaptureRequest.CONTROL_CAPTURE_INTENT_VIDEO_SNAPSHOT : CaptureRequest.CONTROL_CAPTURE_INTENT_STILL_CAPTURE);
                 // n.b., don't set RequestTagType.CAPTURE here - we only do it for the last of the burst captures (see below)
                 camera_settings.setupBuilder(stillBuilder, true);
                 if( use_fake_precapture_mode && fake_precapture_torch_performed ) {
